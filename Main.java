67c05bd55b9f6b689092608517a2e39b628018eb
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2024-06-14 10:50:30 +0100

    learning diference between preferences values

--- Main.java
+++ Main.java
@@ -5,16 +5,21 @@ import java.util.Date;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        byte age = 30;
-        long viewsCount = 12_233_444_67_899L;
-        double price = 10.3456678;
-        float discount = 10.23F;
-        char letter = 'a';
-        String string = "hjk";
-        System.out.println(age);
-        boolean availability = true;
-        Date now = new Date();
-        System.out.println(now);
+//        byte age = 30;
+//        long viewsCount = 12_233_444_67_899L;
+//        double price = 10.3456678;
+//        float discount = 10.23F;
+//        char letter = 'a';
+//        String string = "hjk";
+//        System.out.println(age);
+//        boolean availability = true;
+//        Date now = new Date();
+//        System.out.println(now);
+byte x=1;
+byte y=x;
+x=3;
+        System.out.println(x);
+        System.out.println(y);
 
 
         for (int i = 1; i <= 6; i++) {
