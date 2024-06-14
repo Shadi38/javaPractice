64bd6351cfa7e85253438c39842160517e6f1662
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2024-06-14 14:34:56 +0100

    learning constants

--- Main.java
+++ Main.java
@@ -1,6 +1,4 @@
-import java.awt.*;
 import java.util.Arrays;
-import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -61,10 +59,51 @@ public class Main {
 //        int[] numbers = new int[5];
 //        numbers[0] = 1;
 //        numbers[1] = 2;
-        int[] numbers = {1,2,3,4,5};
-        System.out.println(numbers);//it shows the address of array in memory
+        int[] numbers = {3,1,2,4,5};
+        System.out.println(numbers);//it shows the string which is the address
+        // of this object (array) in the memory
         System.out.println(Arrays.toString(numbers));
+        //array have a fix length
         System.out.println(numbers.length);
+        //we can sort the array
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+        //multi-dimensional-array
+        //two-dimensional-array for example having 2 rows and 3 columns
+//        int[][] matrix = new int[2][3];
+//        matrix[0][0] = 1;//assign first row and first column 1
+        int[][] matrix = {{1,2,3},{4,5,6}};
+        System.out.println(Arrays.toString(matrix));//show us the memory address that matrix is saved
+        System.out.println(Arrays.toString(matrix[0]));
+        System.out.println(Arrays.deepToString(matrix));//is showing first and second rows
+        //three-dimensional-array
+        int[][][] cube = new int[2][3][5];
+        System.out.println(Arrays.deepToString(cube));
+
+        // constants(sabet yani we can't change their value trough the code and should be capital)
+         final float PI = 3.141592653589793238462643f;
+
+         //arithmetic expressions +  -  *  /
+        int result1 = 3*5;
+        System.out.println(result1);
+        //result without float numbers
+        int result2 = 16/5;
+        System.out.println(result2);
+        //result with float number>>> we should convert numbers to double
+        double result3 = (double)16 / (double)5;
+        System.out.println(result3);
+        //++
+        int a=1;
+        int b=a++;
+        System.out.println(a);
+        System.out.println(b);
+        int c=1;
+        int d =++c;
+        System.out.println(c);
+        System.out.println(d);
+        a = a + 2;// same as a+=2 or a-=2  or a*=2  or  a/=2
+
 
 
 
