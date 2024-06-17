0e07d493d5429bf5cfd806f080dfc14212e7c876
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2024-06-17 13:02:18 +0100

    learning casting

--- Main.java
+++ Main.java
@@ -26,10 +26,10 @@ public class Main {
 //        System.out.println(point2.x);
 
 //        String message = new String("Hello");
-          String message = "  HELLO World" + "!!  ";
+          String message = "  HELLO World" + "!! ";
           System.out.println(message);
           //using the endsWith and startsWith method of the String class and returns boolean
-        System.out.println(message.endsWith("!!"));
+        System.out.println(message.endsWith("!! "));
         System.out.println(message.startsWith("!!"));
         System.out.println(message.length());
         System.out.println(message.indexOf("H"));
@@ -37,9 +37,10 @@ public class Main {
         System.out.println(message.lastIndexOf("w"));
         //replace method doesn't change the original message and return the new string
         System.out.println(message.replace("!", "*"));
-        System.out.println(message);
+
         //the lowerCase method doesn't change the original message and return the new string
         System.out.println(message.toLowerCase());
+        System.out.println(message);
         //with trim method we can get rid of extra white spaces that can
         // be at the beginning or at the end of string
         System.out.println(message.trim());
@@ -81,7 +82,7 @@ public class Main {
         int[][][] cube = new int[2][3][5];
         System.out.println(Arrays.deepToString(cube));
 
-        // constants(sabet yani we can't change their value trough the code and should be capital)
+        // constants(sabet yani we can't change their value through the code and should be capital)
          final float PI = 3.141592653589793238462643f;
 
          //arithmetic expressions +  -  *  /
@@ -104,6 +105,26 @@ public class Main {
         System.out.println(d);
         a = a + 2;// same as a+=2 or a-=2  or a*=2  or  a/=2
 
+        //implicit casting(converting)--> yani agar short ke 2 byte ro ba int(5) ke 4 byte jam konim
+        // dar java automatically short tabdil be int(ke bite bishtari dare) mishe
+        //byte>short>int>long>float>double
+        short x=1;
+        int   y=x+5;
+
+        double e=1.1;
+        double f=e + 2;//result is 3.1 --> baraye jame double to int(2), int tabdil be double mishe(2.0)
+
+        double g=1.1;
+        int h= (int)g + 2;//result is 3 --> chon h int ast bayad g tabdil be int beshe(1) va bad ba 2 jam beshe
+
+        //we can not cast string to integer, we have to convert string to integer
+        String l="1";
+        int r=Integer.parseInt(l) + 2;
+        System.out.println(r);
+        //agar addadike bekhaym add konim ashar dashte bashe baraye tabdil be adad bayad az kelase dige mesle Double or Float estefade konim
+        String o= "1.1";
+        double p= Double.parseDouble(o) + 2;
+        System.out.println(p);
 
 
 
